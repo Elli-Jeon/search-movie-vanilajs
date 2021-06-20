e3915d9e188be77892402c54bd2fa0c3b36030e9
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-06-20 14:27:32 +0900

    Show fetched data on screen
    동적으로 생성

--- fetch.js
+++ fetch.js
@@ -3,6 +3,7 @@ const fetchData = async (keyword) => {
     let url = new URL(`https://cors-anywhere.herokuapp.com/https://openapi.naver.com/v1/search/movie.json`);
     let params = {
         query : `${keyword}`,
+        display : 20,
         // genre : [1,2],
     }
     let requestOptions = {
@@ -16,7 +17,7 @@ const fetchData = async (keyword) => {
     try{
         let response = await fetch(url,requestOptions);
         let data = await response.json();
-        console.log(data.items);
+        return (data.items);
     }catch(error){
         throw new Error(error);
     } 
@@ -27,16 +28,65 @@ const fetchData = async (keyword) => {
 const searchBar = document.querySelector(".searchBar");
 const submitBtn = document.querySelector(".submitBtn")
 
+/* fetch한 데이터를 화면에 표시 */
+const showMovieDataOnScreen = (movieData) => {
+    const movieDataSection = document.querySelector('.movieData');
+    console.log(movieData);
+
+    movieData.forEach((data)=>{
+        const eachMovieSection = document.createElement('div'); // 각 movie가 들어갈 div
+        // image section
+        const imageSec = document.createElement('img');
+        imageSec.src = data.image;
+        imageSec.alt = "image";
+        eachMovieSection.appendChild(imageSec);
+        // info section
+        const infoSec = document.createElement('div');
+        const title = document.createElement('h3');
+        const director = document.createElement('h4');
+        const actor = document.createElement('h4');
+        const pubDate = document.createElement('h4');
+        
+        title.innerHTML = data.title; // innerHTML 쓰기는 싫은뎅..
+        infoSec.appendChild(title);
+        director.innerHTML = data.director;
+        infoSec.appendChild(director);
+        actor.innerHTML = data.actor;
+        infoSec.appendChild(actor);
+        pubDate.innerHTML = data.pubDate;
+        infoSec.appendChild(pubDate);
+        eachMovieSection.appendChild(infoSec);
+        // rating section
+        const rating = document.createElement('div');
+        rating.innerText = Number(data.userRating);
+        eachMovieSection.appendChild(rating);
+        // more section
+        const more = document.createElement('p');
+        const moreContainer = document.createElement('a');
+        more.textContent = "더보기"
+        moreContainer.href = data.link;
+        moreContainer.appendChild(more);
+        eachMovieSection.appendChild(moreContainer);
+
+        movieDataSection.appendChild(eachMovieSection);
+    })
+    
+    console.log(movieData[1].image);
+}
+
 // input에서 엔터키 누르면 바로 되도록.
-searchBar.addEventListener('keyup',(e)=>{
-    if(window.event.keyCode === 13){
-        fetchData(e.target.value);
+searchBar.addEventListener('keyup',async (e)=>{
+    if(window.event.keyCode === 13){ // enter키의 keycode가 13이라고 함.
+        let movieData = await fetchData(e.target.value);
+        showMovieDataOnScreen(movieData);
     }
 })
 
 // submit버튼 누르면 작동
-submitBtn.addEventListener('click',(e)=>{
+submitBtn.addEventListener('click',async (e)=>{
     e.preventDefault();
     const searchTitle = searchBar.value;
-    fetchData(searchTitle);
+    let movieData = await fetchData(searchTitle);
+    showMovieDataOnScreen(movieData);
 })
+
