a6165a285bc369a9e5a34e520e87393e8059f924
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-06-20 12:33:03 +0900

    Add search event

--- fetch.js
+++ fetch.js
@@ -1,3 +1,4 @@
+/* Fetch data */
 const fetchData = async (keyword) => {
     let url = new URL(`https://cors-anywhere.herokuapp.com/https://openapi.naver.com/v1/search/movie.json`);
     let params = {
@@ -21,5 +22,21 @@ const fetchData = async (keyword) => {
     } 
 }
 
-fetchData("해리포터");
 
+/* Add event */
+const searchBar = document.querySelector(".searchBar");
+const submitBtn = document.querySelector(".submitBtn")
+
+// input에서 엔터키 누르면 바로 되도록.
+searchBar.addEventListener('keyup',(e)=>{
+    if(window.event.keyCode === 13){
+        fetchData(e.target.value);
+    }
+})
+
+// submit버튼 누르면 작동
+submitBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const searchTitle = searchBar.value;
+    fetchData(searchTitle);
+})
